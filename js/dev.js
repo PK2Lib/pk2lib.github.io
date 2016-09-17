b1e1697f7f81063b7857f54c2a2d1e62dc8e3a34
Crudelis Diabolus <crudelis.diabolus@gmail.com>
2016-09-18 00:41:20 +0300

    Does this help?
    I did not fix it yet...

---
+++ js/dev.js
@@ -0,0 +1,5 @@
+var backgroundMap = {path: "rooster island 2/level12.map", x: 72, y: 194}; // factory 1
+$(document).ready(function(){
+	document.body.onLoad = preInit();
+	$("a").smoothScroll();
+});
