cda59b8e06fca814041202bf701752f0359b4929
Crudelis Diabolus <crudelis.diabolus@gmail.com>
2016-09-18 00:48:38 +0300

    ... suppose all the paths should be fixed
    before it works

--- js/dev.js
+++ js/dev.js
@@ -1,13 +1,13 @@
 var backgroundMap = {path: "rooster island 2/level12.map", x: 72, y: 194}; // factory 1
 function init_images() {
     if(pk2.cvars.draw_static_backgrounds) {
-        IMAGE1.src = "images/1.png";
-        IMAGE2.src = "images/2.png";
-        IMAGE3.src = "images/3.png";
+        IMAGE1.src = "/images/1.png";
+        IMAGE2.src = "/images/2.png";
+        IMAGE3.src = "/images/3.png";
     }
     IMAGE4.src = "/images/FACTORY3.png";
-    APPLE_SPR.src = "images/apple_sprite.png";
-    pk2.images.SPR_BONS.src = "images/SPR_BONS.png";
+    APPLE_SPR.src = "/images/apple_sprite.png";
+    pk2.images.SPR_BONS.src = "/images/SPR_BONS.png";
 }
 $(document).ready(function(){
 	document.body.onLoad = preInit();
