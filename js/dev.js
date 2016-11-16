27e8cef5b83c2a1f8cb356464443ced2c67249e9
Crudelis Diabolus <crudelis.diabolus@gmail.com>
2016-11-16 14:34:07 +0200

    Removed some unnecessary shit.

--- js/dev.js
+++ js/dev.js
@@ -1,8 +1,6 @@
 var backgroundMap = {path: "rooster island 2/level12.map", x: 72, y: 194}; // factory 1
 
-IMG_BACKGROUND.src = "/images/FACTORY3.png";
-
 $(document).ready(function(){
 	document.body.onLoad = preInit();
-	$("a").smoothScroll();
+//	$("a").smoothScroll();
 });
