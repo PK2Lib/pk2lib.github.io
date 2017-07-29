005870580b415174cbc05a7e20c6f6dd3d130e5a
Joni Puljujärvi <joni.puljujarvi@helsinki.fi>
2017-07-29 22:52:17 +0300

    Refactoring

--- js/dev.js
+++ js/dev.js
@@ -1,6 +1,7 @@
+var pk2assetsroot = "../game_assets/";
+var pk2CustomLibraryRoot = "https://v6p9d9t4.ssl.hwcdn.net/html/420707/";
 var backgroundMap = {path: "rooster island 2/level12.map", x: 72, y: 194}; // factory 1
 
 $(document).ready(function(){
-	document.body.onLoad = preInit();
 //	$("a").smoothScroll();
 });
